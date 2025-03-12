a3c111faec474e072cde4b6a3080634849004392
ravilock <ravi.me.professional@hotmail.com>
2025-03-12 12:11:55 -0300

    chore: make scrip output binary to file and use two diferent IPs for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,13 @@ curl --parallel --parallel-immediate \
   "http://localhost:8888/test" \
   "http://localhost:8888/test"
 
+curl --parallel --parallel-immediate \
+  "http://192.168.0.7:8888/test" \
+  "http://192.168.0.7:8888/test" \
+  "http://192.168.0.7:8888/test" \
+  "http://192.168.0.7:8888/test" \
+  "http://192.168.0.7:8888/test"
+
 echo "Last Request"
 
-curl "http://localhost:8888/api" -i
+curl "http://localhost:8888/api" --output out.bin
